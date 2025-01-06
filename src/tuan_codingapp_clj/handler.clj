42fbedd1afc0942e973dd64dcfeec9f579ab16ec
Le Ngoc Tuan <lengoctuan14032006@gmail.com>
2025-01-06 18:28:46 +0700

    practice postgres database

--- src/tuan_codingapp_clj/handler.clj
+++ src/tuan_codingapp_clj/handler.clj
@@ -10,3 +10,21 @@
 
 (def app
   (wrap-defaults app-routes site-defaults))
+
+(def db {:dbtype "postgres" :dbname "dvdrental"
+         :user "postgres" :password "postgres"
+         :host "127.0.0.1" :port 5432})
+
+(def ds (jdbc/get-datasource db))
+
+(jdbc/execute! ds ["select * from actor"]) 
+
+(jdbc/execute! ds ["
+create table address3 (
+  id serial primary key,
+  name varchar(32),
+  email varchar(255)
+)"])
+
+(jdbc/execute! ds ["insert into address3 (name, email)
+                    values ('lengoctuan', 'tuan@gmail.com')"])
